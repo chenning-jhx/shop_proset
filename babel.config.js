76fef823c7bc44181196eb1cbd1f2962a3aad2c8
chenning <387670951@qq.com>
2021-03-07 10:26:14 +0800

    2021-03-07

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
+//项目发布结算使用的babel插件
+let prodPlugins = [];
+if (process.env.NODE_ENV === 'production') {
+  prodPlugins.push('transform-remove-console')
+}
+
 module.exports = {
   presets: [
     '@vue/app'
-  ]
+  ],
+  //发布产品时的插件数组
+  ...prodPlugins
 }
